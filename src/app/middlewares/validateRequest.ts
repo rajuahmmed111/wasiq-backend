4e55767b2b60498fa4c38809c54a766dd2d048de
Raju Ahmmed <ahraju2003@gmail.com>
2026-01-23 02:12:46 +0600

    create a day trip service

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -5,12 +5,18 @@ const validateRequest =
   (schema: AnyZodObject | ZodEffects<AnyZodObject>) =>
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      await schema.parseAsync({
+      const validationData = await schema.parseAsync({
         body: req.body,
         query: req.query,
         params: req.params,
         cookies: req.cookies,
       });
+
+      // update req.body with transformed data
+      if (validationData.body) {
+        req.body = validationData.body;
+      }
+
       return next();
     } catch (error) {
       next(error);
